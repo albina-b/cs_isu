fc0abfa95efbedda993ca94d73cf56af5046170a
albina-b <58530455+albina-b@users.noreply.github.com>
2019-12-04 16:27:52 -0500

    Add files via upload

---
+++ albina.py
@@ -0,0 +1,48 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello world")
+hello world
+>>> for i in range(10): return i
+SyntaxError: 'return' outside function
+>>> 
+>>> for i in range(10): print(i)
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> for i in range(10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> 
+>>> 
+>>> p
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    p
+NameError: name 'p' is not defined
+>>> 
+=============================== RESTART: Shell ===============================
+>>> print("new project")
+new project
+>>> print("albina babuk")
+albina babuk
+>>> 
